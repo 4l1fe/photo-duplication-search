3a3fc28405be8025c3db870f6267037009c12443
4l1fe <4l1fe@users.noreply.github.com>
2017-01-17 02:41:18 +0300

    remove initial reading, add queue emptiness condition

--- search.py
+++ search.py
@@ -15,7 +15,6 @@ from contextlib import suppress
 LOGGER = 'main'
 DATA_QUEUE_SIZE = 100
 DATA_GETTING_TIMEOUT = 15
-INITAL_DATA_READING_TIMEOUT = 1
 DUMP_DB = 'duplicated.db'
 
 
@@ -26,10 +25,11 @@ def get_file_data(data_queue, logger, absolute_file_name):
     with open(absolute_file_name, 'rb') as file:
         data = file.read()
         data_queue.put((absolute_file_name, data))
-        logger.info('putted')
+        logger.info('putted %d bytes' % len(data))
 
 
 def main(dir, thread_count, dump):
+    logger.debug('thread count: %d' % thread_count)
     pool = Pool(processes=thread_count)
     all_files = []
     data_queue = Queue(DATA_QUEUE_SIZE)
@@ -41,18 +41,18 @@ def main(dir, thread_count, dump):
 
     pget_file_data = functools.partial(get_file_data, data_queue, logger)
     aresult = pool.map_async(pget_file_data, all_files)
-    aresult.wait(INITAL_DATA_READING_TIMEOUT)  # to give the time to threads for the data_queue filling
-    logger.info('waited and pooled all file names')
+    logger.info('pooled all file names')
 
     with suppress(Empty):
-        while not aresult.ready():
+        while not aresult.ready() or not data_queue.empty():
+            logger.debug('data queue size %d' % data_queue.qsize())
             afn, data = data_queue.get(timeout=DATA_GETTING_TIMEOUT)
             hash = hashlib.sha1(data).hexdigest()
             duplicated[hash].append(afn)
-            logger.info('hashed')
             data_queue.task_done()
+            logger.info('hashed')
 
-    duplicated = dict(filter(lambda item: len(item[1]) >= 2 , duplicated.items()))
+    duplicated = dict(filter(lambda item: len(item[1]) >= 2, duplicated.items()))
     if not duplicated:
         logger.info('there are not duplicated photoes')
     elif dump:
@@ -68,7 +68,7 @@ def main(dir, thread_count, dump):
 if __name__ == '__main__':
     arg_parser = argparse.ArgumentParser()
     arg_parser.add_argument('directory')
-    arg_parser.add_argument('-tc', '--thread-count', type=int, default=os.cpu_count()*2)
+    arg_parser.add_argument('-tc', '--thread-count', type=int, default=os.cpu_count())
     arg_parser.add_argument('-dump', action='store_true')
     args = arg_parser.parse_args()
 
